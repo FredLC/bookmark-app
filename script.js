be0acac515b8c686bc865b35b8d39386d88a3f60
fredlc <fredlefevre@outlook.com>
2020-06-30 13:57:51 -0400

    Integrated localStorage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
@@ -24,6 +26,21 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        bookmarks = [
+            {
+                name: 'Google',
+                url: 'https://google.com',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
+
 function storeBookmark(e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
@@ -34,6 +51,15 @@ function storeBookmark(e) {
     if (!validate(nameValue, urlValue)) {
         return false;
     }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 modalShow.addEventListener('click', showModal);
@@ -44,3 +70,5 @@ window.addEventListener('click', (e) =>
     e.target === modal ? modal.classList.remove('show-modal') : false
 );
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+fetchBookmarks();
