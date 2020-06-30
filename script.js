7f533fd53dbeeeddc7467efb649c9335b45502b6
fredlc <fredlefevre@outlook.com>
2020-06-30 13:44:31 -0400

    Form validation

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ function showModal() {
     websiteNameEl.focus();
 }
 
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please provide values for both fields.');
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid URL.');
+        return false;
+    }
+    return true;
+}
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `http://${urlValue}`;
+    }
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () =>
     modal.classList.remove('show-modal')
@@ -18,3 +43,4 @@ modalClose.addEventListener('click', () =>
 window.addEventListener('click', (e) =>
     e.target === modal ? modal.classList.remove('show-modal') : false
 );
+bookmarkForm.addEventListener('submit', storeBookmark);
